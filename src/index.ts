07e4764f08690f355ec1eb88e63f32523a444697
Usva Dhiar Praditya <usva.praditya@solusi247.com>
2021-08-16 03:46:56 +0700

    remove file-drop

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from  './file-upload/file-select.directive';
-export * from  './file-upload/file-drop.directive';
 export * from  './file-upload/file-uploader.class';
 export * from './file-upload/file-item.class';
 export * from './file-upload/file-like-object.class';
